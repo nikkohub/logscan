4d10ab2b03a2b14a13530f039cca3d79b830fbd8
nikkohub <revilo@live.se>
2021-12-16 07:49:46 +0100

    Delete log.sh

--- log.sh
+++
@@ -1,1 +0,0 @@
-curl -k https://github.com/nikkohub/logscan/raw/main/log4j2-scan -o log4j2-scan && chmod +x log4j2-scan && ./log4j2-scan /var && ./log4j2-scan /usr && ./log4j2-scan /bin && ./log4j2-scan /opt && ./log4j2-scan /usr/bin && ./log4j2-scan /etc
